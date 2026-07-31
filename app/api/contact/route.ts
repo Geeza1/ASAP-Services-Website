e82ea0224ddc98c03b41b4fc5b01de6b6593a6c8
Geeza1 <56462397+Geeza1@users.noreply.github.com>
2026-07-31 19:04:45 +1000

    nodemailer changed to true, timeout changed for testing

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
 export const runtime = "nodejs";
-
+export const maxDuration = 90;
 type ContactPayload = {
   name?: unknown;
   phone?: unknown;
@@ -198,14 +198,15 @@ export async function POST(request: Request) {
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: process.env.SMTP_SECURE === "false",
+  secure: process.env.SMTP_SECURE === "true",
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
-  connectionTimeout: 8000,
-  greetingTimeout: 8000,
-  socketTimeout: 10000,
+  //temporary testing form connection issues with vercel, these timeouts are set to higher values
+connectionTimeout: 60_000,
+  greetingTimeout: 60_000,
+  socketTimeout: 90_000,
 });
 
 console.log("Creating SMTP connection...");
