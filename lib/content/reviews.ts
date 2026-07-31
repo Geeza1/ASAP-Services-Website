c73cc177972e9dd37d8a79666b9fa2b0316cc246
Geeza1 <56462397+Geeza1@users.noreply.github.com>
2026-07-31 10:28:32 +1000

    Reviews

--- lib/content/reviews.ts
+++ lib/content/reviews.ts
@@ -13,7 +13,7 @@ export const homepageGoogleReviews: GoogleReview[] = [
     rating: 5,
     reviewText:
       "The ASAP Auto Electrics Team did a get job of installing my new Bluetti Charger 1 on my GWM Tank 300.  Great customer service and support, and reasonably priced!  Very happy to recommend them for any auto electrical work requirements around the Ringwood area.",
-    service: "Electrical diagnostics",
+    service: "Electrical Installation",
     date: "2026",
     sourceLabel: "Google Review"
   },
@@ -22,7 +22,7 @@ export const homepageGoogleReviews: GoogleReview[] = [
     rating: 5,
     reviewText:
       "Would definitely recommend! Very prompt and considerate with explaining and sharing knowledge on how he installed the front and rear dash cam. Job was done fast and efficiently with few visible wires and ensuring all wires don't impact air bags and driving vision.",
-    service: "Auto electrical repair",
+    service: "Electrical Installation",
     sourceLabel: "Google Review"
   },
   {
@@ -30,7 +30,7 @@ export const homepageGoogleReviews: GoogleReview[] = [
     rating: 5,
     reviewText:
       "Absolutely fantastic customer service, they went above and beyond to help me out and their prices are very competitive... Highly recommend and will be using them again.",
-    service: "Workshop service",
+    service: "Workshop Service",
     date: "2026",
     sourceLabel: "Google Review"
   }
